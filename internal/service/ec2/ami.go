2cb633584c54533d1dac142d5aedb4fb4746157f
ritt <mfedotova@croc.ru>
2022-04-28 11:41:54 +0300

    [ritt] st2 + remove resource

--- internal/service/ec2/ami.go
+++ internal/service/ec2/ami.go
@@ -131,7 +131,7 @@ func ResourceAMI() *schema.Resource {
 							Type:         schema.TypeString,
 							Optional:     true,
 							ForceNew:     true,
-							Default:      ec2.VolumeTypeStandard,
+							Default:      ec2.VolumeTypeSt2,
 							ValidateFunc: validation.StringInSlice(ec2.VolumeType_Values(), false),
 						},
 					},

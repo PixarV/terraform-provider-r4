5de21b823fb4c1aa174623d627e9d53424f0bb74
Margarita Fedotova <mfedotova@croc.ru>
2023-09-25 01:25:57 +0300

    paas: implement service resource

---
+++ internal/service/paas/services/util.go
@@ -0,0 +1,39 @@
+package services
+
+import (
+	"log"
+)
+
+const (
+	ServiceTypeElasticSearch = "elasticsearch"
+	ServiceTypeMemcached     = "memcached"
+	ServiceTypePostgreSQL    = "pgsql"
+	ServiceTypeRedis         = "redis"
+)
+
+func ServiceTypeValues() []string {
+	return []string{
+		ServiceTypeElasticSearch,
+		ServiceTypeMemcached,
+		ServiceTypePostgreSQL,
+		ServiceTypeRedis,
+	}
+}
+
+const (
+	ServiceClassCacher   = "cacher"
+	ServiceClassDatabase = "database"
+	ServiceClassSearch   = "search"
+)
+
+// Map with ServiceManager objects for each supported PaaS service.
+var managers = map[string]ServiceManager{}
+
+func GetServiceManager(serviceType string) ServiceManager {
+	if v, ok := managers[serviceType]; ok {
+		return v
+	}
+
+	log.Printf("[ERROR] Can't found parameters manager for service type %s", serviceType)
+	return nil
+}
